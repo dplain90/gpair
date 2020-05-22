960580228fb5458d7a32ccfe8ecafa42ab781c48
Danny Plain <dplain@gmail.com>
2020-05-22 16:41:54 -0400

    feat(dashboard): some message\n\nCoauthored by Danny Plain <dplain@gmail.com>

---
+++ clear-coauthor.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+current_pair_path="/Users/stride-admin/Code/git-coauthor-commit/current-pair.txt"
+rm "$current_pair_path"
---
+++ commit-with-coauthor.sh
@@ -0,0 +1,9 @@
+commit_message=$1
+
+input="/Users/stride-admin/Code/git-coauthor-commit/current-pair.txt"
+read -r current_pair < $input
+
+full_commit_message="$commit_message\n\nCoauthored by $current_pair"
+
+echo $full_commit_message
+git commit -m "$full_commit_message"
